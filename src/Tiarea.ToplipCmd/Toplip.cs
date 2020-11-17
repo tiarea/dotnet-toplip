297fb7438865d09326a2c64ea806f537fd362945
scott <keepnemo@hotmail.com>
2020-11-17 18:13:18 +0800

    test how Process class call toplip

---
+++ src/Tiarea.ToplipCmd/Toplip.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tiarea.ToplipCmd
+{
+    public sealed class Toplip
+    {
+        /// <summary>
+        /// 文件加密
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <param name="password">密码</param>
+        /// <param name="cancelToken"></param>
+        /// <returns>加密文件路径</returns>
+        public static Task<string> EncryptAsync(string filePath, string password, CancellationToken? cancelToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 文件解密
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <param name="password">密码</param>
+        /// <param name="token"></param>
+        /// <returns>解密文件路径</returns>
+        public static Task<string> DecryptAsync(string filePath, string password, CancellationToken? cancelToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 文件隐藏至图片
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="imageFilePath"></param>
+        /// <param name="password"></param>
+        /// <param name="cancelToken"></param>
+        /// <returns></returns>
+        public static Task<string> HiddenFileInImageAsync(string filePath, string imageFilePath, string password, CancellationToken? cancelToken) 
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 从图片中获取图片
+        /// </summary>
+        /// <param name="imageFilePath"></param>
+        /// <param name="password"></param>
+        /// <param name="cancelToken"></param>
+        /// <returns></returns>
+        public static Task<string> ExtractFileFromImage(string imageFilePath, string password, CancellationToken? cancelToken)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ src/Tiarea.ToplipCmd/ToplipContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Tiarea.ToplipCmd
+{
+    class ToplipContext
+    {
+        /// <summary>
+        /// 输入文件路径
+        /// </summary>
+        public string InputFilePath { set; get; }
+        /// <summary>
+        /// 输入文件扩展名
+        /// </summary>
+        public string InputFileExt { get { return Path.GetExtension(InputFileExt); } }
+        /// <summary>
+        /// 图片文件路径，通常在进行图片隐写时使用
+        /// </summary>
+        public string ImgFilePath { set; get; }
+        /// <summary>
+        /// 图片文件扩展名
+        /// </summary>
+        public string ImgFileExt { get { return Path.GetExtension(ImgFilePath); } }
+        /// <summary>
+        /// 输出问文件路径（包括文件名）
+        /// </summary>
+        public string OutputFilePath { set; get; }
+        /// <summary>
+        /// 密码列表
+        /// </summary>
+        public IEnumerable<string> PasswordList { set; get; }
+        /// <summary>
+        /// 密码数量
+        /// </summary>
+        public int PasswordCount { get { return PasswordList.Count(); } }
+    }
+}
